7833b5a6d89e5da76614c81bd7c0f02b5bf8cb25
moosewebdev <agudbranson@gmail.com>
2019-09-25 16:20:16 -0400

    fixed bugs with flickering on nav while scrolling

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
 $(document).ready(function(){
 
     var scrollLink = $('.scroll');
-
+    var navElement = $("nav");
     //smooth scrolling
     scrollLink.click(function(e){
         e.preventDefault();
         $('body,html').animate({scrollTop:$(this.hash).offset().top -75}, 700);
     });
+    
 
-    //active link switch
+    //active link switch hide show nav bg
     $(window).scroll(function(){
+        var scrollbarRealLoc = $(this).scrollTop();
+        if (scrollbarRealLoc > 0 ){
+            navElement.addClass('show');
+        }else{
+            navElement.removeClass('show');
+        }
         var scrollbarLocation = $(this).scrollTop() + 100;
         console.log(scrollbarLocation);
         console.log(scrollbarLocation -75);
         scrollLink.each(function(){
+
             var sectionOffset = $(this.hash).offset().top;
+
             if (sectionOffset <= scrollbarLocation){
-                $('.scroll').removeClass('active');
+                
                 $(this).addClass('active');
+                $(this).siblings().removeClass('active');
+                console.log("hello");
             }else if (scrollbarLocation < 1237) {
                 $('.scroll').removeClass('active');
                 
